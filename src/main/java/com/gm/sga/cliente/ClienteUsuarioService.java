44c51ecca9dc205fea4c6a33fb46cc097eb7a81b
harold <harold@harold-Ubuntu>
2020-10-10 15:31:09 -0600

    Prueba usuario JPQL, sql con JPA.

---
+++ src/main/java/com/gm/sga/cliente/ClienteUsuarioService.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gm.sga.cliente;
+
+import com.gm.sga.domain.Usuario;
+import com.gm.sga.servicio.UsuarioServiceRemote;
+import java.util.List;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author harold
+ */
+public class ClienteUsuarioService {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando llamada al EJB desde el cliente\n");
+        try {
+            Context jndi = new InitialContext();            
+            UsuarioServiceRemote usuarioService = (UsuarioServiceRemote) jndi.lookup("java:global/sga-jee-jpa/UsuarioServiceImpl!com.gm.sga.servicio.UsuarioServiceRemote");
+            List<Usuario> usuarios = usuarioService.listarUsuarios();
+            for (Usuario usuario : usuarios) {
+                System.out.println(usuario);
+            }
+            System.out.println("\nFin llamada al EJB desde el cliente");
+        } catch (NamingException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+}
